e375db3e135cadc02d252e9e14d343998330c748
kagaline <wackiyoriginal@gmail.com>
2022-05-20 22:29:12 +0900

    I solved this problem!!

--- DSL_A_1.py
+++ DSL_A_1.py
@@ -1,45 +1,44 @@
+
 class Sets:
+    '''
+    互いに素な集合のクラス
+    '''
     def __init__(self, n):
-        self.sets = {i:i for i in range(n)}
-        self.counter = n
-
+        '''
+        
+        '''
+        self.number_to_color = {i:i for i in range(n)}
+        self.color = n
+        self.color_to_numbers = {i:[i] for i in range(n)}
+        
     def unite(self, x, y):
-        color1 = self.sets[x]
-        color2 = self.sets[y]
-        if color1 == 0:
-            self.sets[x] = self.counter
-        else:
-            for i in range(len(self.sets)):
-                if self.sets[i] == color1:
-                    self.sets[i] = self.counter
-
-        if color2 == 0:
-            self.sets[y] = self.counter
-        else:
-            for i in range(len(self.sets)):
-                if self.sets[i] == color2:
-                    self.sets[i] = self.counter
-
-        #print(self.sets)
-        self.counter += 1
+        '''
+        集合同士を結合する
+        '''
+        if self.same(x, y):
+            return 0
+        color1 = self.number_to_color[x]
+        color2 = self.number_to_color[y]
+        self.color_to_numbers[self.color] = self.color_to_numbers[color1] + self.color_to_numbers[color2]
+        for number in self.color_to_numbers[self.color]:
+            self.number_to_color[number] = self.color
+        self.color += 1
+    
+    def same(self, x, y):
+        '''
+        '''
+        if self.number_to_color[x] == self.number_to_color[y]:
+            return 1
         return 0
 
-    def same(self, x, y):
-        if self.sets[y] == self.sets[x]:
-                return int(1)
-        return int(0)
 
-    
-n, q = map(int, input().split(" "))
+n,q = map(int, input().split(" "))
 
-sets1 = Sets(n)
+set1 = Sets(n)
 
 for _ in range(q):
     com, x, y = map(int, input().split(" "))
-
     if com == 0:
-        sets1.unite(x, y)
+        set1.unite(x, y)
     elif com == 1:
-        print(sets1.same(x, y))
-
-
+        print(set1.same(x, y))
