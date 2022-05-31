bf087b36e406bb4cd0c23157e37aeb4d8ab7f2e6
kagaline <wackiyoriginal@gmail.com>
2022-05-31 15:50:17 +0900

    commit for pushing

--- ALDS1/ALDS1_1_D.py
+++ ALDS1/ALDS1_1_D.py
@@ -4,12 +4,3 @@
 n = int(input())
 
 # int: rate
-rate = [int(input()) for _ in range(n)]
-
-max_diff = -(10**9)
-for j in range(1, n):
-    for i in range(0, j):
-        if rate[j] - rate[i] > max_diff:
-            max_diff = rate[j] - rate[i]
-
-print(max_diff)
