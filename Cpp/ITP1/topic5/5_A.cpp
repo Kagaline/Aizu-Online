0173cecb437987dfa7eab60638b241bd52cf6fa4
Kagaline <809Kagaline@users.noreply.github.com>
2024-06-06 10:23:52 +0900

    update: add topic 5 solutions.

---
+++ Cpp/ITP1/topic5/5_A.cpp
@@ -0,0 +1,29 @@
+#include <iomanip>
+#include <iostream>
+
+int main(void)
+{
+  int H, W;
+
+  while (true)
+  {
+    std::cin >> H >> W;
+
+    if (H == 0 && W == 0)
+    {
+      break;
+    }
+
+    for (int y = 0; y < H; ++y)
+    {
+      for (int x = 0; x < W; ++x)
+      {
+        std::cout << "#";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+  return 0;
+}
---
+++ Cpp/ITP1/topic5/5_D.cpp
@@ -0,0 +1,34 @@
+#include <iomanip>
+#include <iostream>
+
+int main(void)
+{
+  int n;
+  std::cin >> n;
+
+  for (int i = 1; i <= n; ++i)
+  {
+    int x = i;
+
+    if (x % 3 == 0)
+    {
+      std::cout << " " << i;
+      continue;
+    }
+
+    while (x)
+    {
+      if (x % 10 == 3)
+      {
+        std::cout << " " << i;
+        break;
+      }
+
+      x /= 10;
+    }
+  }
+
+  std::cout << std::endl;
+
+  return 0;
+}
